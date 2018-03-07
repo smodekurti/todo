c3245c3c751720f48e993920e00dc8f06a8d3ec1
Vasu Modekurti <Srinivas_Modekurti@baxter.com>
2018-03-07 15:56:04 -0600

    Updated text Input

--- app/components/NewToDoItem/NewToDoItem.js
+++ app/components/NewToDoItem/NewToDoItem.js
@@ -5,11 +5,19 @@ import {StyleSheet, View, TextInput, TouchableOpacity, Dimensions, Text} from 'r
 import Styles from './Styles';
 
 class NewToDoItem extends Component {
+    handleNewToDo(todo){
+
+    }
     render(){
         return(
             <View style={Styles.container}>
-                <TextInput style={Styles.input}/>
-                <TouchableOpacity style={Styles.buttonContainer}>
+                <TextInput 
+                style={Styles.input}
+                placeholder="New ToDo"
+                placeholderTextColor="#636e72"
+
+                />
+                <TouchableOpacity style={Styles.buttonContainer} onPress={() => this.handleNewToDo}>
                     <Text style={Styles.text}>Add</Text>
                 </TouchableOpacity>
             </View>
